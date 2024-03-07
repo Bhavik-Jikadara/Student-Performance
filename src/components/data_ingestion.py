437f3ccfa28b384c7990b9aaa915bd3d3ca578b8
Bhavik Jikadara <bhavikjikadara33523@gmail.com>
2024-03-07 21:39:14 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,12 @@
 import os
 import sys
+from src.components import data_transformation
 from src.exception import CustomException
 from src.logger import logging
+
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -28,8 +33,7 @@ class DataIngestion:
             os.makedirs(os.path.dirname(
                 self.ingestion_config.train_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,
-                      index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train test split initiated")
             train_set, test_set = train_test_split(
@@ -55,3 +59,10 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_tansformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+
